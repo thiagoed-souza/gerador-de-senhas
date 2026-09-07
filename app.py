21d82b1877399d90e8092024c515cc6407db8b52
Thiago Gabriel <150717336+thiagoed-souza@users.noreply.github.com>
2026-09-07 23:30:12 +0000

    Atualização da porta

--- app.py
+++ app.py
@@ -208,4 +208,4 @@ def perfil():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
